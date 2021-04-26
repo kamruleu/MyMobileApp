944c3bcdab0c94eccc2cf7b2844ba7708507075d
M Kamrul Islam <kamrulfci2014@gmail.com>
2021-04-27 00:06:48 +0600

    Adding Delete and Close Icon

--- src/components/PlaceDetail/PlaceDetail.js
+++ src/components/PlaceDetail/PlaceDetail.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, Text, Button, Modal, Image} from 'react-native';
+import {View, Text, Button, Modal, Image, TouchableOpacity} from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 const PlaceDetail = props => {
@@ -7,14 +7,18 @@ const PlaceDetail = props => {
         <Modal>
             <Image source={props.place.image} style={{width: "100%", height: 200}} />
             <Text style={{textAlign: 'center', fontSize: 25}}>{props.place.value}</Text>
-            <View>
-                <Icon name="trash" size={30} color="red" />
-                <Button title="Delete" color="red" onPress={()=>
+            <View style={{alignItems:'center'}}>
+                <TouchableOpacity onPress={()=>
                     {
                         props.handleDeletePlace(props.place.key)
                     }
-                    } />
-                <Button title="Close" onPress={()=>props.handleHideModal()} />
+                    } >
+                    <Icon name="trash" size={60} color="red" />
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={()=>props.handleHideModal()} >
+                    <Icon name="times-circle" size={60} color="#4C6EF5" />
+                </TouchableOpacity>
             </View>
         </Modal>
     )
